bdb4a0ad5ea05fb2b4e218c96d086f608b5f121d
Ricarth Lima <rrsl@cin.ufpe.br>
2022-06-09 19:10:02 -0300

    v1.3 - erros x excecoes

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -1,25 +1,32 @@
-import 'controllers/bank_controller.dart';
-import 'models/account.dart';
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+//
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+//
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+//
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+//
+//   // Observando resultado
+//   print(result);
+// }
 
 void main() {
-  // Criando o banco
-  BankController bankController = BankController();
+  double amount = double.parse("25,33"); // FormatException
+  recursiveFunction(1); // StackOverflowError
+}
 
-  // Adicionando contas
-  bankController.addAccount(
-      id: "Ricarth",
-      account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
-
-  bankController.addAccount(
-      id: "Kako",
-      account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
-
-  // Fazendo transferência
-  bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-  // Observando resultado
-  print(result);
+recursiveFunction(int count) {
+  print(count);
+  recursiveFunction(count + 1);
 }
