f9a71251facbd5152f24b0212769504453c6103f
Ricarth Lima <rrsl@cin.ufpe.br>
2022-06-14 03:40:22 -0300

    v4.3 - simulacao

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -1,24 +1,19 @@
+import 'dart:math';
+
 import 'controllers/bank_controller.dart';
 import 'exceptions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
 void testingNullSafety() {
-  print("My String".runtimeType);
-  print(null.runtimeType);
-  Null;
+  Account? myAccount;
 
-  String cantBeNull = "My String";
-  //cantBeNull = null;
-
-  String? canBeNull = "My String";
-  print(canBeNull.runtimeType);
-  canBeNull = null;
-  print(canBeNull.runtimeType);
-
-  Account? myNullAccount = null; // Explicar o warning
-  //print(myNullAccount.balance); // Explicar o erro
-  print(
-      myNullAccount!.balance); // Mostrar o problema, contextualizar no passado
+  // Simulando uma comunicação externa que pode ou não preencher myAccount
+  Random rng = Random();
+  if (rng.nextInt(10) % 2 == 0) {
+    myAccount =
+        Account(name: "Ricarth Lima", balance: 300, isAuthenticated: true);
+  }
+  print(myAccount.runtimeType);
 }
 
 void main() {
