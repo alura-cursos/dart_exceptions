253930680b5b52f036649aef7b082a831a566805
Ricarth Lima <rrsl@cin.ufpe.br>
2022-06-14 00:45:53 -0300

    v3.1 - throw

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -22,38 +22,6 @@
 // }
 
 void main() {
-  int i = 0;
-  print("Started Main");
-  function01();
-  print("Finished Main");
-}
-
-function01() {
-  int j = 25;
-  print("Started F01");
-  //function02(j); Usaremos essa linha para relembrar o erro estourando
-  try {
-    function02(j);
-  } catch (e, s) {
-    print(e.toString());
-    print(s);
-    rethrow;
-  } finally {
-    print(
-        "Essa linha será executada independente de se capturar ou não o erro.");
-  }
-
-  print("Finished F01");
-}
-
-function02(int otherJ) {
-  int k = 0;
-  print("Started F02");
-
-  for (int i = 1; i <= 5; i++) {
-    print(i);
-    double amount = double.parse("Not a number.");
-  }
-  otherJ = 50;
-  print("Finished F02");
+  throw FormatException();
+  throw "Ocorreu um erro"; // Funciona, mas não é um padrão nas linguagens.
 }
